d58c289e7336b2c76500da80fa6a8278350594a5
Frédéric GAURAT <fgaurat@gmail.com>
2026-05-12 17:13:34 +0200

    end day 02

--- tp_scrap/main.py
+++ tp_scrap/main.py
@@ -1,6 +1,7 @@
 import requests
 from readability import Document
 from markdownify import markdownify as md
+from ollama import chat
 
 def main():
     url = "https://scie-cablage.com"
@@ -10,7 +11,7 @@ def main():
     with open("categories.txt", "r", encoding="utf-8") as file:
         categories = file.read().splitlines()
 
-        
+
     # print(response.text)
     # with open("scie-cablage.html", "w", encoding="utf-8") as file:
     #     file.write(response.text)
@@ -19,5 +20,17 @@ def main():
     markdown = md(clean_html)
     with open("scie-cablage.md", "w", encoding="utf-8") as file:
         file.write(markdown)
+
+    response = chat(model='gemma3', messages=[
+        {
+            'role': 'system',
+            'content': f'You are a helpful assistant that categorizes the content of the website {url} into the following categories: {categories}.'
+        },
+    {
+        'role': 'user',
+        'content': 'Here the content of the website in markdown format:\n\n' + markdown,
+    },
+    ])
+    print(response.message.content)        
 if __name__ == '__main__':
     main()
